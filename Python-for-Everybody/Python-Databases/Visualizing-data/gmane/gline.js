dea2ad9ce071ea38517fcbd4d59af1f8537f77dd
NestorMonroy <nestor.monroy.90@gmail.com>
2021-01-26 13:20:59 -0600

    adding examples to visializing data

---
+++ Python-for-Everybody/Python-Databases/Visualizing-data/gmane/gline.js
@@ -0,0 +1,3 @@
+gline = [ ['Month','unicon.net','cam.ac.uk','umich.edu','rsmart.com','mtu.edu','virginia.edu','gmail.com','northwestern.edu','mac.com','suny.edu'],
+['2005-12',4,3,2,2,2,2,2,2,1,1]
+];
---
+++ Python-for-Everybody/Python-Databases/Visualizing-data/pagerank/spider.js
@@ -0,0 +1,22 @@
+spiderJson = {"nodes":[
+{"weight":2,"rank":10.200870511425459, "id":1, "url":"http://www.dr-chuck.com"},
+{"weight":3,"rank":19.0, "id":2, "url":"http://www.dr-chuck.com/dr-chuck/resume/index.htm"},
+{"weight":1,"rank":3.8041349292709454, "id":3, "url":"http://www.dr-chuck.com/sakai-book"},
+{"weight":1,"rank":0.07029379760609133, "id":4, "url":"http://www.dr-chuck.com/dr-chuck/resume/bio.htm"},
+{"weight":1,"rank":0.07029379760609133, "id":9, "url":"http://www.dr-chuck.com/dr-chuck/resume/leadership.htm"},
+{"weight":1,"rank":0.07029379760609133, "id":11, "url":"http://www.dr-chuck.com/dr-chuck/resume/pictures/index.htm"},
+{"weight":2,"rank":0.07029379760609133, "id":12, "url":"http://www.dr-chuck.com/dr-chuck/resume"},
+{"weight":1,"rank":0.0, "id":30, "url":"http://www.dr-chuck.com/dr-chuck/resume/pictures"}],
+"links":[
+{"source":0,"target":1,"value":3},
+{"source":0,"target":2,"value":3},
+{"source":1,"target":0,"value":3},
+{"source":1,"target":3,"value":3},
+{"source":1,"target":4,"value":3},
+{"source":1,"target":5,"value":3},
+{"source":1,"target":6,"value":3},
+{"source":3,"target":1,"value":3},
+{"source":3,"target":7,"value":3},
+{"source":4,"target":1,"value":3},
+{"source":6,"target":0,"value":3},
+{"source":6,"target":6,"value":3}]};
