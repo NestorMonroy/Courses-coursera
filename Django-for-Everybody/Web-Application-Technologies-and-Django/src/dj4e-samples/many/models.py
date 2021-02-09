0563bffaed392e5d3e62251fc452d9bfb66f7015
NestorMonroy <nestor.monroy.90@gmail.com>
2021-02-08 19:54:53 -0600

    examples js

--- Django-for-Everybody/Web-Application-Technologies-and-Django/src/dj4e-samples/many/models.py
+++ Django-for-Everybody/Web-Application-Technologies-and-Django/src/dj4e-samples/many/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Person(models.Model):
     email = models.CharField(max_length=128, unique=True)
     name = models.CharField(max_length=128, null=True)
@@ -8,12 +9,14 @@ class Person(models.Model):
     def __str__(self):
         return self.email
 
+
 class Course(models.Model):
     title = models.CharField(max_length=128, unique=True)
     members = models.ManyToManyField('Person', through='Membership')
 
     def __str__(self):
         return self.title
+
 
 class Membership(models.Model):
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
@@ -26,20 +29,20 @@ class Membership(models.Model):
     ADMIN = 10000
 
     MEMBER_CHOICES = (
-        ( LEARNER, 'Learner'),
-        ( IA, 'Instructional Assistant' ),
-        ( GSI, 'Grad Student Instructor' ),
-        ( INSTRUCTOR, 'Instructor' ),
-        ( ADMIN, 'Administrator' ),
+        (LEARNER, 'Learner'),
+        (IA, 'Instructional Assistant'),
+        (GSI, 'Grad Student Instructor'),
+        (INSTRUCTOR, 'Instructor'),
+        (ADMIN, 'Administrator'),
     )
-    
+
     role = models.IntegerField(
         choices=MEMBER_CHOICES,
         default=LEARNER,
     )
 
     def __str__(self):
-        return "Person "+ str(self.person.id) + " <--> Course " + str(self.course.id)
+        return "Person " + str(self.person.id) + " <--> Course " + str(self.course.id)
 
 # References
 
@@ -50,4 +53,3 @@ class Membership(models.Model):
 # https://docs.djangoproject.com/en/3.0/topics/db/examples/many_to_many/
 
 # https://docs.djangoproject.com/en/3.0/ref/models/fields/#datefield
-
