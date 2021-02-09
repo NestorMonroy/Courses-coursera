1fce145c8de48ad1af1a70c29db2a8ce8b1b236e
NestorMonroy <nestor.monroy.90@gmail.com>
2021-02-08 21:09:07 -0600

    adding ads 2

--- Django-for-Everybody/Web-Application-Technologies-and-Django/src/django_nestor/mysite/ads/views.py
+++ Django-for-Everybody/Web-Application-Technologies-and-Django/src/django_nestor/mysite/ads/views.py
@@ -1,5 +1,12 @@
-from .models import Ad
+from django.views import View
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Ad, Comment
 from .owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
+from .forms import CreateForm, CommentForm
 
 
 class AdListView(OwnerListView):
@@ -10,17 +17,90 @@ class AdListView(OwnerListView):
 
 class AdDetailView(OwnerDetailView):
     model = Ad
+    template_name = "ads/ad_detail.html"
+
+    def get(self, request, pk):
+        x = Ad.objects.get(id=pk)
+        comments = Comment.objects.filter(ad=x).order_by('-updated_at')
+        comment_form = CommentForm()
+        context = {'ad': x, 'comments': comments,
+                   'comment_form': comment_form}
+        return render(request, self.template_name, context)
 
 
-class AdCreateView(OwnerCreateView):
-    model = Ad
-    fields = ['title', 'price', 'text']
+class AdCreateView(LoginRequiredMixin, View):
+    template_name = 'ads/ad_form.html'
+    success_url = reverse_lazy('ads:all')
+
+    def get(self, request, pk=None):
+        form = CreateForm()
+        ctx = {'form': form}
+        return render(request, self.template_name, ctx)
+
+    def post(self, request, pk=None):
+        form = CreateForm(request.POST, request.FILES or None)
+
+        if not form.is_valid():
+            ctx = {'form': form}
+            return render(request, self.template_name, ctx)
+
+        ad = form.save(commit=False)
+        ad.owner = self.request.user
+        ad.save()
+        return redirect(self.success_url)
 
 
-class AdUpdateView(OwnerUpdateView):
-    model = Ad
-    fields = ['title', 'price', 'text']
+class AdUpdateView(LoginRequiredMixin, View):
+    template_name = 'ads/ad_form.html'
+    success_url = reverse_lazy('ads:all')
+
+    def get(self, request, pk):
+        ad = get_object_or_404(Ad, id=pk, owner=self.request.user)
+        form = CreateForm(instance=ad)
+        ctx = {'form': form}
+        return render(request, self.template_name, ctx)
+
+    def post(self, request, pk=None):
+        ad = get_object_or_404(Ad, id=pk, owner=self.request.user)
+        form = CreateForm(request.POST, request.FILES or None, instance=ad)
+
+        if not form.is_valid():
+            ctx = {'form': form}
+            return render(request, self.template_name, ctx)
+
+        ad = form.save(commit=False)
+        ad.save()
+
+        return redirect(self.success_url)
 
 
 class AdDeleteView(OwnerDeleteView):
     model = Ad
+
+
+def stream_file(request, pk):
+    pic = get_object_or_404(Ad, id=pk)
+    response = HttpResponse()
+    response['Content-Type'] = pic.content_type
+    response['Content-Length'] = len(pic.picture)
+    response.write(pic.picture)
+    return response
+
+
+class CommentCreateView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        a = get_object_or_404(Ad, id=pk)
+        comment = Comment(
+            text=request.POST['comment'], owner=request.user, ad=a)
+        comment.save()
+        return redirect(reverse('ads:ad_detail', args=[pk]))
+
+
+class CommentDeleteView(OwnerDeleteView):
+    model = Comment
+    template_name = "ads/comment_delete.html"
+
+    # https://stackoverflow.com/questions/26290415/deleteview-with-a-dynamic-success-url-dependent-on-id
+    def get_success_url(self):
+        ad = self.object.ad
+        return reverse('ads:ad_detail', args=[ad.id])
