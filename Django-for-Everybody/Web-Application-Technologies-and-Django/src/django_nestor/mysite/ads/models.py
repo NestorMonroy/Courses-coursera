5da0896635a4d8ade31b5962be1b47e51b93db65
NestorMonroy <nestor.monroy.90@gmail.com>
2021-02-10 14:56:51 -0600

    adding Favorites

--- Django-for-Everybody/Web-Application-Technologies-and-Django/src/django_nestor/mysite/ads/models.py
+++ Django-for-Everybody/Web-Application-Technologies-and-Django/src/django_nestor/mysite/ads/models.py
@@ -18,6 +18,8 @@ class Ad(models.Model):
                                     help_text='The MIMEType of the file')
     comments = models.ManyToManyField(settings.AUTH_USER_MODEL,
                                       through='Comment', related_name='comments_owned')
+    favorites = models.ManyToManyField(settings.AUTH_USER_MODEL,
+        through='Fav', related_name='favorite_ads')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -44,3 +46,15 @@ class Comment(models.Model):
         if len(self.text) < 15:
             return self.text
         return self.text[:11] + ' ...'
+
+
+class Fav(models.Model) :
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    # https://docs.djangoproject.com/en/3.0/ref/models/options/#unique-together
+    class Meta:
+        unique_together = ('ad', 'user')
+
+    def __str__(self) :
+        return '%s likes %s'%(self.user.username, self.ad.title[:10])
--- Django-for-Everybody/Web-Application-Technologies-and-Django/src/django_nestor/mysite/ads/views.py
+++ Django-for-Everybody/Web-Application-Technologies-and-Django/src/django_nestor/mysite/ads/views.py
@@ -3,8 +3,12 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+# csrf exemption in class based views
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.db.utils import IntegrityError
 
-from .models import Ad, Comment
+from .models import Ad, Comment, Fav
 from .owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
 from .forms import CreateForm, CommentForm
 
@@ -12,7 +16,19 @@ from .forms import CreateForm, CommentForm
 class AdListView(OwnerListView):
     model = Ad
     # By convention:
-    # template_name = "ads/ad_list.html"
+    template_name = "ads/ad_list.html"
+    #template_name = "favs/list.html"
+
+    def get(self, request):
+        ad_list = Ad.objects.all()
+        favorites = list()
+        if request.user.is_authenticated:
+            # rows = [{'id': 2}, {'id': 4} ... ]  (A list of rows)
+            rows = request.user.favorite_ads.values('id')
+            # favorites = [2, 4, ...] using list comprehension
+            favorites = [row['id'] for row in rows]
+        ctx = {'ad_list': ad_list, 'favorites': favorites}
+        return render(request, self.template_name, ctx)
 
 
 class AdDetailView(OwnerDetailView):
@@ -104,3 +120,32 @@ class CommentDeleteView(OwnerDeleteView):
     def get_success_url(self):
         ad = self.object.ad
         return reverse('ads:ad_detail', args=[ad.id])
+
+
+# csrf exemption in class based views
+# https://stackoverflow.com/questions/16458166/how-to-disable-djangos-csrf-validation
+
+@method_decorator(csrf_exempt, name='dispatch')
+class AddFavoriteView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        print("Add PK", pk)
+        a = get_object_or_404(Ad, id=pk)
+        fav = Fav(user=request.user, ad=a)
+        try:
+            fav.save()  # In case of duplicate key
+        except IntegrityError as e:
+            pass
+        return HttpResponse()
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class DeleteFavoriteView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        print("Delete PK", pk)
+        a = get_object_or_404(Ad, id=pk)
+        try:
+            fav = Fav.objects.get(user=request.user, ad=a).delete()
+        except Fav.DoesNotExist as e:
+            pass
+
+        return HttpResponse()
